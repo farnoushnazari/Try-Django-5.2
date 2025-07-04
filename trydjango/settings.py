23b5f35d2960db33e1a9acd26b6198da675045dc
farnoushnazari <farnoushnazari98@gmail.com>
2025-07-05 00:31:49 +0330

    Use .env to hide valuable data

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#p1_7u=v^)ec=1^_*c!y6xjoo5)aj*au$6f^&)9+2f*tn9z-zb"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG', '0')) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('HOST').split(',')
 
 
 # Application definition
