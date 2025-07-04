03da1b83c34549dc405019d52ada0a0a041b7500
farnoushnazari <farnoushnazari98@gmail.com>
2025-07-04 20:55:16 +0330

    Use Model in ArticleForm

--- articles/views.py
+++ articles/views.py
@@ -29,9 +29,7 @@ def article_create_view(request):
     }
     print(form)
     if form.is_valid():
-        title = form.cleaned_data.get('title')
-        content = form.cleaned_data.get('content')
-        article_object = Article.objects.create(title=title, content=content)
+        article_object = form.save()
         context['object'] = article_object
         context['created'] = True
     return render(request, "articles/create.html", context=context)
