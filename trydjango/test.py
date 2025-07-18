bb6550a119e4db539b130fb6d8eb6b9c84c45546
farnoushnazari <farnoushnazari98@gmail.com>
2025-07-18 20:16:45 +0330

    write Test for checking secret key

---
+++ trydjango/test.py
@@ -0,0 +1,13 @@
+import os
+from django.contrib.auth.password_validation import validate_password
+from django.test import TestCase
+
+class TryDjangoConfigTest(TestCase):
+    def test_secret_key_strength(self):
+        # settings.SECRET_KEY
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+        try:
+            is_strong = validate_password(SECRET_KEY)
+        except Exception as e:
+            msg = f'Weak Secret Key {e.messages}'
+            self.fail(msg)
