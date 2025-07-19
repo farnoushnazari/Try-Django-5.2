a611cd04455f5f126995ab8d70d62a934dd8dbd2
farnoushnazari <farnoushnazari98@gmail.com>
2025-07-19 22:15:17 +0330

    Add signals to clean models

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify 
+from django.db.models.signals import pre_save, post_save
 
 # Create your models here.
 class Article(models.Model):
@@ -11,6 +12,20 @@ class Article(models.Model):
     publish = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.slug is None:
-            self.slug = slugify(self.title)
+        # if self.slug is None:
+        #     self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+def article_pre_save(sender, instance, *args, **kwargs):
+    print('pre_save')
+    print(args, kwargs)
+    if instance.slug is None:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(article_pre_save, sender=Article)
+
+def article_post_save(*args, **kwargs):
+    print('post_save')
+    print(args, kwargs)
+
+post_save.connect(article_post_save, sender=Article)
